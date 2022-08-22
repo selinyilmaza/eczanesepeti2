bf39f73579289b8001f9bcb8cb6a85e6e3d9d629
selinyilmaza <selinyilmazb@gmail.com>
2022-08-22 17:50:42 +0300

    tema değişimi

--- Data/Migrations/20220822020400_Tablolar4.Designer.cs
+++ Data/Migrations/20220822020400_Tablolar4.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eczanesepeti2.Data;
 
 namespace eczanesepeti2.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220822020400_Tablolar4")]
+    partial class Tablolar4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20220822020400_Tablolar4.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eczanesepeti2.Data.Migrations
+{
+    public partial class Tablolar4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IlId",
+                table: "Ilce",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Il",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ad = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Il", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ilce_IlId",
+                table: "Ilce",
+                column: "IlId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ilce_Il_IlId",
+                table: "Ilce",
+                column: "IlId",
+                principalTable: "Il",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ilce_Il_IlId",
+                table: "Ilce");
+
+            migrationBuilder.DropTable(
+                name: "Il");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ilce_IlId",
+                table: "Ilce");
+
+            migrationBuilder.DropColumn(
+                name: "IlId",
+                table: "Ilce");
+        }
+    }
+}
