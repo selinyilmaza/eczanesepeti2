f65b830de1a3b3c24cb58558c2175050ba6d0fba
selinyilmaza <selinyilmazb@gmail.com>
2022-08-24 12:58:49 +0300

    Çoklu dil (bitmedi)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using eczanesepeti2.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -16,15 +17,18 @@ namespace eczanesepeti2.Controllers
     public class HomeController : Controller
     {
         private readonly ApplicationDbContext _context;
-        
+        private readonly IStringLocalizer<HomeController> _localizer;
 
-        public HomeController(ApplicationDbContext context)
+
+        public HomeController(ApplicationDbContext context, IStringLocalizer<HomeController> localizer)
         {
             _context = context;
+            _localizer = localizer;
         }
 
         public IActionResult Index()
         {
+            var say_Hello_value = _localizer["Say_Hello"];
             var db = _context.Ilac
                 .Include(f => f.Kategori);
 
--- Startup.cs
+++ Startup.cs
@@ -5,12 +5,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -36,8 +39,32 @@ namespace eczanesepeti2
             services.AddDefaultIdentity<Kisi>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddLocalization(options => { options.ResourcesPath = "Resources"; });
+            services.Configure<RequestLocalizationOptions>(
+                 options =>
+                 {
+                     var supportedCultures = new List<CultureInfo>
+                     {
+                         new CultureInfo("en-US"),
+                         new CultureInfo("tr-TR"),
+
+
+
+                     };
+                     options.DefaultRequestCulture = new RequestCulture(culture: "tr-TR");
+                     options.SupportedCultures = supportedCultures;
+                     options.SupportedUICultures = supportedCultures;
+
+                     options.RequestCultureProviders = new List<IRequestCultureProvider>
+                     {
+                         new QueryStringRequestCultureProvider(), //querystring
+                         new CookieRequestCultureProvider(), // Cookie
+                         new AcceptLanguageHeaderRequestCultureProvider() //Header
+                     };
+                 });
         }
 
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -59,6 +86,9 @@ namespace eczanesepeti2
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            var options = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(options.Value);
 
             app.UseEndpoints(endpoints =>
             {
